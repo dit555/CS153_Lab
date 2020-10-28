15c1d7865df5c23292a154a2ef4787969f5b373c
Dumitru Chiriac <dchir002@sledge.cs.ucr.edu>
2020-10-27 19:44:53 -0700

    waitpid(...) has been added

--- proc.c
+++ proc.c
@@ -312,6 +312,52 @@ wait(int *status)
   }
 }
 
+// Wait for a child process to exit and return its pid.
+// Return -1 if this process has no children.
+int
+waitpid(int pid, int *status, int options)
+{
+  struct proc *p;
+  int havekids, pidRet;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE && (p->pid == pid || pid == 0)){ //check if pid of exiting is the one we are looking for if 0 any pid is accepted
+        // Found one.
+        pidRet = p->pid;
+	status = &p->exitStatus;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pidRet;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+
+
 //PAGEBREAK: 42
 // Per-CPU process scheduler.
 // Each CPU calls scheduler() after setting itself up.
--- sysproc.c
+++ sysproc.c
@@ -27,6 +27,12 @@ sys_wait(int *status)
 }
 
 int
+sys_waitpid(int pid, int *status, int options)
+{
+  return waitpid(pid, status, options);
+}
+
+int
 sys_kill(void)
 {
   int pid;
