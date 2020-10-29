4d77881986d1e3d87318ab097781eddc1d56b0ba
Dumitru Chiriac <56750709+dit555@users.noreply.github.com>
2020-10-29 14:25:18 -0700

    Update test11.c

--- test11.c
+++ test11.c
@@ -6,20 +6,38 @@
 int
 main(int argc, char *argv[])
 {
-  int pid, pid2;
-  int status;
+  int pid, ret_pid, exit_status;
+    int i;
+    // use this part to test exit(int status) and wait(int* status)
 
-  pid = fork();
-  if (pid == 0){
-	printf(1, "exiting pid %d with 6\n", getpid());
-	exit(6);
-  }
-  else if (pid > 0) {  
-  	pid2 = wait(&status);
-  	printf(1, "child %d exited with status: %d\n", pid2, status);
-  }
-  else{
-	printf(1, "fork failure\n");
-  }
-  exit(0);
+    printf(1, "\n  Parts a & b) testing exit(int status) and wait(int* status):\n");
+
+    for (i = 0; i < 2; i++)
+    {
+        pid = fork();
+        if (pid == 0)
+        { // only the child executed this code
+            if (i == 0)
+            {
+                printf(1, "\nThis is child with PID# %d and I will exit with status %d\n", getpid(), 0);
+                exitS(0);
+            }
+            else
+            {
+                printf(1, "\nThis is child with PID# %d and I will exit with status %d\n", getpid(), -1);
+                exitS(-1);
+            }
+        }
+        else if (pid > 0)
+        { // only the parent executes this code
+            ret_pid = wait(&exit_status);
+            printf(1, "\n This is the parent: child with PID# %d has exited with status %d\n", ret_pid, exit_status);
+        }
+        else // something went wrong with fork system call
+        {
+            printf(2, "\nError using fork\n");
+            exitS(-1);
+        }
+    }
+    return 0;
 }
